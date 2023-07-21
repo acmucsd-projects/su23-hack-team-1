14678d79c7893eac4c78e4ddbacf0ebcd7e83cc8
pviwatkurkul <pviwatkurkul@ucsd.edu>
2023-07-20 19:52:21 -0700

    installing dependencies

---
+++ client/src/Front Page Components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react"
+import {useLocation, useNavigate} from 'react-router-dom';
+
+function Home (){
+    const location = useLocation()
+
+    return (
+        <div className="homepage">
+
+            <h1>Hello {location.state.id}</h1>
+        </div>
+    )
+}
