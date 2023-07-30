429826b4375a2646e1dca403de60aaa450b58556
pviwatkurkul <pviwatkurkul@ucsd.edu>
2023-07-30 14:36:33 -0700

    general set up

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const router = express.Router();
+// const mongoose = require("mongoose");
 
+// const UserSchema = new mongoose.Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//   },
+//   firstname: {
+//     type: String, 
+//     required: true,
+//   },
+//   age: {
+//     type: Number,
+//     required:true,    
+//   }
+// });
+
+// const UserModel = mongoose.model("users", UserSchema)
+// module.exports = UserModel;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   const user = {
