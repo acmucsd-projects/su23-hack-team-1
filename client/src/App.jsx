f54454b255c26122de2cd5cf04f16ddf33544a85
sriya p <122697432+sriya224@users.noreply.github.com>
2023-08-19 19:26:28 -0700

    created navbar component with styling
    coded the html for navbar in navbar.jsx and created navbar.css to style it. adjusted App.jsx to allow for routing to navbar file to temporarily test how it looks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Signup from './Components/Signup'
 import Login from './Components/Login'
 import Home from './Components/Home'
+import Navbar from './Components/navbar'
 import{BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App(){
@@ -10,6 +11,7 @@ function App(){
         <Route path="/Signup" element={<Signup />}></Route>
         <Route path="/Login" element={<Login />}></Route>
         <Route path="/Home" element={<Home />}></Route>
+        <Route path="/navbar" element={<Navbar />}></Route>
       </Routes>
     </BrowserRouter>
   )
