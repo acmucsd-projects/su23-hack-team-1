22874c887ec7287acd75aa17efec281b66a67e42
Claire <claire.andolina@gmail.com>
2023-08-24 22:08:11 -0700

    Base .html and .css completed

--- client/src/Components/Pantry.jsx
+++ client/src/Components/Pantry.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React from 'react';
+import "./ItemList.css";
 
 function Pantry(){
     return(
-        <h2>Pantry</h2>
+        <body class="page-theme">
+        <table class="fixed_header">
+        <thead>
+          <tr>
+            <th class="col1name">Name:</th>
+            <th class="col2purchased">Purchased:</th>
+            <th class="col3expires">Expires:</th>
+            <th class="col4amount">Amount:</th>
+            <th class="col5button"><div><button>Sort By:</button></div></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="col1name">dfgh</td>
+            <td class="col2purchased"> gg</td>
+            <td class="col3expires">g</td>
+            <td class="col4amount"> gg</td>
+            <td class="col5button"> h</td>
+          </tr>
+          <tr>
+            <td class="col1name">g</td>
+            <td class="col2purchased">  g</td>
+            <td class="col3expires">g</td>
+            <td class="col4amount"> gg</td>
+            <td class="col5button"> h</td>
+          </tr>
+          <tr>
+            <td class="col1name">g</td>
+            <td class="col2purchased">  g</td>
+            <td class="col3expires">g</td>
+            <td class="col4amount"> gg</td>
+            <td class="col5button"> h</td>
+          </tr>
+          <tr>
+            <td class="col1name">g</td>
+            <td class="col2purchased">  g</td>
+            <td class="col3expires">g</td>
+            <td class="col4amount"> gg</td>
+            <td class="col5button"> h</td>
+          </tr>
+          <tr>
+            <td class="col1name">g</td>
+            <td class="col2purchased">  g</td>
+            <td class="col3expires">g</td>
+            <td class="col4amount"> gg</td>
+            <td class="col5button"> h</td>
+          </tr>
+        </tbody>
+      </table>
+      </body>
     )
 }
 
