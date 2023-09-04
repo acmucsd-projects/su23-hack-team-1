f30ce83661c77215edfd9ed87e603516454bbf52
sriya p <122697432+sriya224@users.noreply.github.com>
2023-09-04 09:43:58 -0700

    Add Item Form
    Completed UI for the add item form. Used react date picker instead of calendar; may need to install it on your terminal.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Signup from './Components/Signup'
 import Login from './Components/Login'
 import Home from './Components/Home'
 import Pantry from './Components/Pantry'
+import Dropdownbox from './Components/Dropdownbox'
 import{BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App(){
@@ -12,6 +13,7 @@ function App(){
         <Route path="/Login" element={<Login />}></Route>
         <Route path="/Home" element={<Home />}></Route>
         <Route path="/Pantry" element={<Pantry/>}></Route>
+        <Route path="/Dropdownbox" element={<Dropdownbox/>}></Route>
 
       </Routes>
     </BrowserRouter>
--- client/src/Components/Dropdownbox.jsx
+++ client/src/Components/Dropdownbox.jsx
@@ -1,74 +1,130 @@
+import React from 'react';
 import { useState } from "react";
-import Calendar from "react-calendar";
+import "./Dropdownbox.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { FaCalendarAlt } from 'react-icons/fa';
 
-const create = () =>{
+
+function Dropdownbox(){
     const [name, setUserName] = useState('');
     const [selectitem, setSelectitem] = useState('');
     const [Amount, setAmount] = useState('');
     const [Fridge, setFridge] = useState('');
-    const [date, setDate] = useState(new Date())
+    const [purchaseDate, setPurchaseDate] = useState(null);
+    const [expiryDate, setExpiryDate] = useState(null);
+    const [checked, setChecked] = React.useState(false);
+    
+    const handlePurchaseDateChange = (date) => {
+        setPurchaseDate(date);
+    };
 
-    return(
-        <div className="create">
-            <form>
-                <lable>name</lable>
-                <input 
-                   type = "name"
-                   required
-                   value = {name}
-                   onChange = {(e)=>setUserName(e.target.value)}
-                />
-                <label>Select Item Type</label>
-                <select
-                    value = {selectitem}
-                    onChange = {(e)=> setSelectitem(e.target.value)}
-                />
-                    <option value = "meat">meat</option>
-                    <option value = "poultry">poultry</option>
-                    <option value = "vegetables">vegetables</option>
-                    <option value = "fruit">fruit</option>
-                <select
-                    value = {Fridge}
-                    onChange={(e)=> setFridge(e.target.value)}
-                />
-                
-                <label>Store in Fridge ? </label>
-                <select>
-                     <option value = "Yes">Yes</option>
-                     <option value = "No">No</option>
-                     <option value = "Deep Fridge">Deep Fridge</option>
-                 </select>
-                 <label>Purchase date</label>
-                     <div className="calendar-container">
-                     <Calendar onChange={setDate} value={date}/> 
-                     </div>
-                 <div className="text-center">
-                     Selected date: {date.toDateString()}
-                     </div>
-                 <label>expiry date</label>
-                     <div className="calendar-container">
-                     <Calendar onChange={setDate} value={date}/> 
-                     </div>
-                 <div className="text-center">
-                     Selected date: {date.toDateString()}
-                     </div>
-     
-                <label>Amount</label>
-                <input
-                     type= "Amount"
-                     required
-                     vaule = {Amount}
-                     onChange ={(e)=>setAmount(e.target.value)}
-                />
-                
+    const handleExpiryDateChange = (date) => {
+        setExpiryDate(date);
+    };
+
+    const handleChange = () => {
+        setChecked(!checked);
+    };
+
+        return(
+            <div className="Add-Item-Container">
+                <div className = "Add-Item-Form">
                     
-                
-                 
+                        <div className = "Item-div">
+                        <label className = "Item">Item: </label>
+                        
+                        <input className = "name"
+                            type = "text"
+                            placeholder = "Name"
+                            required
+                            value = {name}
+                            onChange = {(e)=>setUserName(e.target.value)}
+                        />
+                        </div>
+                        
+                        <div className = "form">
+                        <label>
+                            <p>Category</p>
+                        <select
+                            value = {selectitem}
+                            onChange = {(e)=> setSelectitem(e.target.value)}
+                        >
+                            <option key="" value="">
+                            Select Item
+                            </option>
+                            <option value = "Meat">meat</option>
+                            <option value = "Poultry">poultry</option>
+                            <option value = "Vegetables">vegetables</option>
+                            <option value = "Fruit">fruit</option>
+                        </select>
+                        </label>
 
-        
-            </form>
+                        <label> 
+                            <p>Location</p>
+                        <select
+                         value = {Fridge}
+                         onChange={(e)=> setFridge(e.target.value)}
+                         >
+                            <option key="" value="">
+                            Store in fridge?
+                            </option>
+                            <option value = "Yes">Yes</option>
+                            <option value = "No">No</option>
+                            <option value = "Deep Fridge">Deep Fridge</option> 
+                        </select>
+                        </label>
+                        <div>
+                        <label><p>Purchase Date:</p></label>
+                        <DatePicker
+                        selected={purchaseDate}
+                        onChange={handlePurchaseDateChange}
+                        dateFormat="MM/dd/yyyy"
+                        placeholderText="mm/dd/yyyy"
+                        showYearDropdown // Enable year dropdown
+                        scrollableYearDropdown // Allow scrolling through years
+                        />
+                    </div>
+                    <div>
+                        <label>
+                            <p>Expiry Date:</p>
+                        </label>
+                        <DatePicker
+                        selected={expiryDate}
+                        onChange={handleExpiryDateChange}
+                        dateFormat="MM/dd/yyyy"
+                        placeholderText="mm/dd/yyyy"
+                        showYearDropdown // Enable year dropdown
+                        scrollableYearDropdown // Allow scrolling through years
+                        icon = {FaCalendarAlt}
+                        />
+                        
 
-        </div>
-    )
+                    </div>
+            
+                        <label>
+                            <p>Amount</p>
+                        <input
+                            vaule = {Amount}
+                            onChange ={(e)=>setAmount(e.target.value)}
+                            placeholder = "3 apples; 2 lb; etc."
+                        />
+                        </label>
+                        </div>
+
+                        <label className = "checkbox-component">
+                    
+                        <input className = "checkbox-input"
+                        type="checkbox"
+                        checked={checked}
+                        onChange={handleChange}
+                        />
+                        Choose given expiry date: 
+                    </label>
+ 
+                </div>
+            </div>
+        )
 }
+
 export default Dropdownbox;
