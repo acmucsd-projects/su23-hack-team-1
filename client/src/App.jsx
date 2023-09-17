a2ead67f868682a63c8a2d354a157ac9e7649cfd
sriya p <122697432+sriya224@users.noreply.github.com>
2023-09-17 00:32:50 -0700

    finished main UI of add item box
    Added submit button and finished styling; renamed files to add-item-box. may need to adjust when merging with main as the sign-up and log-in pages have conflicting CSS global names.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import Signup from './Components/Signup'
 import Login from './Components/Login'
 import Home from './Components/Home'
 import Pantry from './Components/Pantry'
-import Dropdownbox from './Components/Dropdownbox'
+import Add from './Components/add-item-box'
 import{BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App(){
@@ -13,7 +13,7 @@ function App(){
         <Route path="/Login" element={<Login />}></Route>
         <Route path="/Home" element={<Home />}></Route>
         <Route path="/Pantry" element={<Pantry/>}></Route>
-        <Route path="/Dropdownbox" element={<Dropdownbox/>}></Route>
+        <Route path="/Add" element={<Add/>}></Route>
 
       </Routes>
     </BrowserRouter>
