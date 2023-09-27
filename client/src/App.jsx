6293b8fcae210e6e91efe54accf4870d0a117448
pviwatkurkul <pviwatkurkul@ucsd.edu>
2023-09-26 20:03:08 -0700

    Merging drop down with pantry

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Signup from './Components/Signup'
 import Login from './Components/Login'
 import Home from './Components/Home'
 import Pantry from './Components/Pantry'
+import Add from './Components/add-item-box'
 import{BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 function App(){
@@ -9,9 +10,10 @@ function App(){
     <BrowserRouter>
       <Routes>
         <Route path="/Signup" element={<Signup />}></Route>
-        <Route path="/Login" element={<Login />}></Route>
+        <Route path="/Login" element={<Login />}></Route> 
         <Route path="/Home" element={<Home />}></Route>
         <Route path="/Pantry" element={<Pantry/>}></Route>
+        <Route path="/Add" element={<Add/>}></Route>
         <Route path="" element={<Navigate to="/Login" />} />
       </Routes>
     </BrowserRouter>
